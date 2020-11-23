fc5fe0a49d687c6d375e737d54f04ac1c0b6f4fc
Matheus Franco <matheus_2222@hotmail.com>
2020-11-23 18:16:09 -0300

    Adicionando uma suite de testes

---
+++ src/test/java/br/com/cartao/proposta/PropostaSuiteTest.java
@@ -0,0 +1,10 @@
+package br.com.cartao.proposta;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("br.com.cartao.proposta")
+public class PropostaSuiteTest {
+}
